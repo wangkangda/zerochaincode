4b598031f565b2541d53ffbe670ad3b9926390a3
wangkangda <wangkangda@pku.edu.cn>
2017-03-14 01:39:59 +0800

    fix bug in tutorial

--- goclient/tutorial.go
+++ goclient/tutorial.go
@@ -1,70 +1,65 @@
 package main
 /*
 #cgo CFLAGS: -l./lib
-#cgo LDFLAGS: -L./lib -lhello -lzerocoin -Wl,-rpath,./lib/
+#cgo LDFLAGS: -L./lib -lzerocoin -Wl,-rpath,./lib/
+#include <stdlib.h>
 #include "Goapi.h"
-#include "hello.h"
 */
 import "C"
 import "fmt"
-import "os"
-import "errors"
-import "strconv"
 import "encoding/json"
+import "unsafe"
 
-func getResp( resp []byte ) map[string]interface{} {
+func getResp( resp []byte ) string {
     var data map[string]interface{}
     if err:=json.Unmarshal( resp, &data ); err!=nil{
         check(err)
     }
     fmt.Println(data)
-    return data
+    result := data[ "result" ].(map[string]interface{})
+    message := result[ "message" ].(string)
+    return message
 }
 
-func Init( params []string ){
+func Init( params []string )[]string{
     depReq := ReqDeploy()
-    resp, err := httpPostForm( depReq )
-    check( err )
+    resp := httpPostForm( depReq )
     dat := getResp( resp )
-    params = append( params, string(dat[ 'result' ][ 'message' ]) )
+    params = append( params, string(dat) )
 
-    queReq := ReqQuery( params[0], 'params' )
-    resp, err = httpPostForm( depReq )
-    check( err )
+    queReq := ReqQuery( params[0], "params" )
+    resp = httpPostForm( queReq )
     dat1 := getResp( resp )
-    params = append( params, string(dat1[ 'result' ][ 'message' ] ) )
+    params = append( params, string(dat1) )
 
-    acuReq := ReqQuery( params[0], 'accumlator' )
-    resp, err = httpPostForm( acuReq )
-    check( err )
+    acuReq := ReqQuery( params[0], "accumlator" )
+    resp = httpPostForm( acuReq )
     dat2 := getResp(resp)
-    params = append( params, string(dat2[ 'result' ][ 'message' ] ) )
+    params = append( params, string(dat2 ) )
 
     return params
 }
 
 func Coinbase( params []string ){
-    baseReq := ReqCoinbase( params[0], 'testuser1', 100 )
-    resp , err := httpPostForm( baseReq )
+    baseReq := ReqCoinbase( params[0], "testuser1", 100 )
+    resp := httpPostForm( baseReq )
     data := getResp(resp)
-    check( err )
+    fmt.Println( data )
 }
 
 func Mint( params []string, ){
-    oParams := C.CCParamsLoad( params[1] )
+    p1 := C.CString( params[1] )
+    //defer C.free( unsafe.Pointer(p1) )
+    oParams := C.CCParamsLoad( p1 )
     defer C.CCParamsDel( oParams )
-    oAccum := C.CCAccumLoad( params[2] )
-    defer C.CCAccumDel( oAccum )
     oPricoin := C.CCPricoinGen( oParams )
     defer C.CCPricoinDel( oPricoin )
     commint := C.CCPubcoinGen( oParams, oPricoin )
     defer C.CCStrDel( commint )
-    accum := C.CCAccumCal( oParams, oAccum, commint )
-    defer C.CCStrDel( accum )
-    params[2] = C.GoString( accum )
 
-    mintReq := ReqMint( params[0], C.GoString(commint), 'No implement')
-    resp, err := httpPostForm( mintReq )
-    check( err )
+    mintReq := ReqMint( params[0], C.GoString(commint), "No implement")
+    resp := httpPostForm( mintReq )
     fmt.Println(resp)
+
+    defer C.free( unsafe.Pointer(p1) )
 }

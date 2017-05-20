a52e7cd280300d625278a7db5e3b18b3e5c9e993
wangkangda <wangkangda@pku.edu.cn>
2017-05-20 10:54:15 +0800

    add client command

--- zeroTrans/client/main.go
+++ zeroTrans/client/main.go
@@ -3,6 +3,7 @@ package main
 import(
     "os"
     "fmt"
+    "bufio"
     "github/wangkangda/zerochaincode/zeroTrans/client/onchain"
     "github/wangkangda/zerochaincode/zeroTrans/client/storage"
 )
@@ -11,16 +12,27 @@ func main(){
     storage.GetStorage()
     defer storage.SaveStorage()
 
-    arg_num :=  len(os.Args)
-    if arg_num <= 1{
-        fmt.Println("Please Input Command!")
+    reader := bufio.NewReader( os.Stdin )
+    for true {
+        fmt.Println("Please Input Command:")
+        input, err := reader.ReadBytes('\n')
+        if err != nil{
+            fmt.Printf("Get %v while input\n", err)
+        }
+        cmd := strings.Split(string(input[0:len(input)-1]), ' ')
+        if len(cmd)==0 {
+            continue
+        }
+        function := cmd[0]
+        fmt.Println("Get cmd:", cmd)
+        switch cmd{
+        case "init":
+            continue
+        case "quit":
+            break
+        default:
+            fmt.Println("Not implement function")
+            continue
+        }
     }
-
-    function = os.Args[1]
-    switch function{
-    case "Init":
-        onchain.Init()
-    }
-
-
 }
